a56c592a58a2ce8f2b1946f64455be8a45a67984
Hizkia Reppi <hizkiajefren0501@gmail.com>
2025-01-23 15:02:59 +0800

    chore: Setup project with laravel breeze api

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return response()->json([
+        'status' => true,
+        'code' => 200,
+        'message' => 'Hello, Welcome to SI Admin PTIK'
+    ], 200);
+});
+
+Route::prefix('v1')->group(function () {
+    Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+        return $request->user();
+    });
+    
+    require __DIR__.'/auth.php';
+});
