e691580773800678cb2de855af4cc5437728fe87
Hizkia Reppi <hizkiajefren0501@gmail.com>
2025-01-26 20:48:01 +0800

    feat: add ApiResponseHelper and LecturerHelper for pagination and NIDN/NIP generation

---
+++ tests/Feature/Models/ResearchFieldTest.php
@@ -0,0 +1,69 @@
+<?php
+
+use App\Models\Lecturer;
+use App\Models\Lecturers\ResearchField;
+use Illuminate\Database\QueryException;
+
+it('can create a research field', function () {
+    $researchField = ResearchField::factory()->create([
+        'field_name' => 'Research Field A',
+        'description' => 'Description of Research Field A'
+    ]);
+
+    expect($researchField)
+        ->toBeInstanceOf(ResearchField::class)
+        ->field_name->toBe('Research Field A')
+        ->description->toBe('Description of Research Field A');
+});
+
+it('throws an error when creating research field without field_name', function () {
+    ResearchField::factory()->create(['field_name' => null]);
+})->throws(QueryException::class);
+
+it('can associate a research field with a lecturer', function () {
+    $lecturer = Lecturer::factory()->create();
+    $researchField = ResearchField::factory()->create([
+        'field_name' => 'Research Field A',
+        'description' => 'Description of Research Field A'
+    ]);
+
+    $researchField->lecturer()->attach($lecturer);
+
+    $this->assertDatabaseHas('lecturer_research_fields', [
+        'lecturer_id' => $lecturer->id,
+        'research_field_id' => $researchField->id
+    ]);
+});
+
+it('can retrieve associated lecturers for a research field', function () {
+    $lecturer = Lecturer::factory()->create();
+    $researchField = ResearchField::factory()->create();
+
+    $researchField->lecturer()->attach($lecturer);
+
+    $lecturers = $researchField->lecturer;
+    expect($lecturers->first()->id)->toBe($lecturer->id);
+});
+
+
+it('ensures research field can be deleted', function () {
+    $researchField = ResearchField::factory()->create();
+
+    $researchField->delete();
+
+    expect(ResearchField::find($researchField->id))->toBeNull();
+});
+
+it('ensures associated records are deleted when a research field is deleted', function () {
+    $lecturer = Lecturer::factory()->create();
+    $researchField = ResearchField::factory()->create();
+
+    $researchField->lecturer()->attach($lecturer);
+
+    $researchField->delete();
+
+    $this->assertDatabaseMissing('lecturer_research_fields', [
+        'lecturer_id' => $lecturer->id,
+        'research_field_id' => $researchField->id
+    ]);
+});
