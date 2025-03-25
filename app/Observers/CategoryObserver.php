21b9a18a587ff9d4699abf540f523b7c280bc8bc
Hizkia Reppi <hizkiajefren0501@gmail.com>
2025-03-25 18:14:51 +0800

    feat: update factories and seeders for Education, Category, and Requirement models; add CategoryObserver for cache management

---
+++ app/Observers/CategoryObserver.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Category;
+use Illuminate\Support\Facades\Cache;
+use Spatie\ResponseCache\Facades\ResponseCache;
+
+class CategoryObserver
+{
+    /**
+     * Handle the Category "created" event.
+     */
+    public function created(Category $category): void
+    {
+        if ($category->role === 'admin') {
+            $this->clearCache($category);
+        }
+    }
+
+    /**
+     * Handle the Category "updated" event.
+     */
+    public function updated(Category $category): void
+    {
+        if ($category->role === 'admin') {
+            $this->clearCache($category);
+        }
+    }
+
+    /**
+     * Handle the Category "deleted" event.
+     */
+    public function deleted(Category $category): void
+    {
+        if ($category->role === 'admin') {
+            $this->clearCache($category);
+        }
+    }
+
+    /**
+     * Handle the Category "restored" event.
+     */
+    public function restored(Category $category): void
+    {
+        if ($category->role === 'admin') {
+            $this->clearCache($category);
+        }
+    }
+
+    /**
+     * Handle the Category "force deleted" event.
+     */
+    public function forceDeleted(Category $category): void
+    {
+        if ($category->role === 'admin') {
+            $this->clearCache($category);
+        }
+    }
+
+    /**
+     * Clear cache
+     */
+    private function clearCache(Category $category): void
+    {
+        $cacheKeys = Cache::get('categories_cache_keys', []);
+
+        foreach ($cacheKeys as $key) {
+            Cache::forget($key);
+        }
+
+        Cache::forget('categories_cache_keys');
+        Cache::forget("category_{$category->id}");
+
+        ResponseCache::clear();
+    }
+}
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,51 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $categories = [
+            [
+                'name' => 'SK Seminar Proposal',
+                'slug' => 'sk-seminar-proposal',
+                'docs_file_path' => null,
+            ],
+            [
+                'name' => 'SK Ujian Hasil Penelitian',
+                'slug' => 'sk-ujian-hasil-penelitian',
+                'docs_file_path' => null,
+            ],
+            [
+                'name' => 'Berita Acara Konversi Nilai MBKM',
+                'slug' => 'berita-acara-konversi-nilai-mbkm',
+                'docs_file_path' => null,
+            ],
+            [
+                'name' => 'Permohonan Ujian Komprehensif',
+                'slug' => 'permohonan-ujian-komprehensif',
+                'docs_file_path' => null,
+            ],
+            [
+                'name' => 'Surat Aktif Kuliah',
+                'slug' => 'surat-aktif-kuliah',
+                'docs_file_path' => null,
+            ],
+            [
+                'name' => 'Ijin Survey',
+                'slug' => 'ijin-survey',
+                'docs_file_path' => null,
+            ],
+            [
+                'name' => 'PDPT',
+                'slug' => 'pdpt',
+                'docs_file_path' => null,
+            ],
+            [
+                'name' => 'Permohonan SK Pembimbing Skripsi',
+                'slug' => 'permohonan-sk-pembimbing-skripsi',
+                'docs_file_path' => null,
+            ],
+        ];
+
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
     }
 }
